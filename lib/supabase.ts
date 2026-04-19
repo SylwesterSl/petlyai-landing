ab17ff9335e90f643261cd228258e81c4202ab02
SylwesterSl <apartserwis9@gmail.com>
2026-04-19 12:55:31 +0200

    Update Supabase URL and ANON KEY

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://mslnptcmvciwyxwqjvmi.supabase.co";
+const SUPABASE_URL = "https://vncvpsobkgvpwcxzvsfr.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zbG5wdGNtdmNpd3l4d3Fqdm1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxNzMzMTYsImV4cCI6MjA4OTc0OTMxNn0.8-6sGetqRaub5T-A0M5ttY5-ZKlNt2tCF1GKK85P_XY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuY3Zwc29ia2d2cHdjeHp2c2ZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNDg5MTYsImV4cCI6MjA5MTkyNDkxNn0.5Ej8c7x_CNJJ4N_0QbbBSHEqDb4CHD3a1NzsXDpezvM";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: false },
