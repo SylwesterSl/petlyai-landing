ffde3af33c09c79472d32b69ee2dacc473604446
SylwesterSl <apartserwis9@gmail.com>
2026-04-26 20:50:01 +0200

    Remove GET function and revalidation settings
    Remove unused GET function and related imports.

--- components/tiles/route.ts
+++ components/tiles/route.ts
@@ -1,8 +1,1 @@
-import { NextResponse } from "next/server";
-import { getTiles } from "@/lib/cms";
 
-export const revalidate = 60;
-
-export async function GET() {
-  return NextResponse.json(await getTiles());
-}
